89a0a58fcfb5e03b8c44df8d01cce2063964d9c0
badhrinadh-a <badhrinadh2002@gmail.com>
2026-06-06 12:50:23 +0000

    chore: downgrade Spark version to 4.1.1 in multiple files

--- airflow/dags/ingestion_dag.py
+++ airflow/dags/ingestion_dag.py
@@ -30,7 +30,7 @@ with DAG(
         export PYTHONPATH=/opt/airflow && \
         spark-submit \
             --master spark://spark-master:7077 \
-            --packages io.delta:delta-core_2.12:4.2.0,org.apache.hadoop:hadoop-aws:3.3.4,com.amazonaws:aws-java-sdk-bundle:1.12.262,org.apache.spark:spark-sql-kafka-0-10_2.12:4.1.2 \
+            --packages io.delta:delta-core_2.12:4.2.0,org.apache.hadoop:hadoop-aws:3.3.4,com.amazonaws:aws-java-sdk-bundle:1.12.262,org.apache.spark:spark-sql-kafka-0-10_2.12:4.1.1 \
             /opt/airflow/src/spark/streaming_ingestion.py
         '''
     )
