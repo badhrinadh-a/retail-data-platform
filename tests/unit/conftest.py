6f921edb731e4c01a896acc59d9bda6431ff8b22
badhrinadh-a <badhrinadh2002@gmail.com>
2026-06-06 11:45:27 +0000

    revert

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -5,22 +5,6 @@ A single session-scoped SparkSession with Delta Lake support avoids conflicts
 when multiple test modules each try to create their own session.
 """
 
-import sys
-import typing
-
-# Workaround for Python 3.12+ compatibility with older PySpark versions (like 3.4.1)
-# PySpark tries to import BinaryIO/TextIO from typing.io,
-# which was removed in Python 3.12.
-try:
-    import typing.io
-except ModuleNotFoundError:
-    import types
-
-    typing_io = types.ModuleType("typing.io")
-    typing_io.BinaryIO = typing.BinaryIO
-    typing_io.TextIO = typing.TextIO
-    sys.modules["typing.io"] = typing_io
-
 import pytest
 from pyspark.sql import SparkSession
 
